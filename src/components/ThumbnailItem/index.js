54bb5caca543f2f85a3871c4a295811fcb8af86e
Saikrishna-Chaitanya-CH <krishnachaitu616@gmail.com>
2022-12-05 13:31:10 +0000

    first commit

---
+++ src/components/ThumbnailItem/index.js
@@ -0,0 +1,26 @@
+import {Component} from 'react'
+import './index.css'
+
+class ThumbnailItem extends Component {
+  render() {
+    const {imageDetails, onClickImage} = this.props
+    const {id, thumbnailUrl, thumbnailAltText} = imageDetails
+    const onClickThumbnail = () => {
+      onClickImage(id)
+    }
+
+    return (
+      <li className="thumbnail" onClick={onClickThumbnail}>
+        <button className="image-button" type="button">
+          <img
+            src={thumbnailUrl}
+            alt={thumbnailAltText}
+            className="thumbnail-image"
+          />
+        </button>
+      </li>
+    )
+  }
+}
+
+export default ThumbnailItem
